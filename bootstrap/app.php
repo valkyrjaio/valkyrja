e55a3e6dd4abbd90f97db215dcc9332c7bae82eb
Melech Mizrachi <melechmizrachi@gmail.com>
2016-11-23 12:16:27 -0800

    Cleanup.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -187,7 +187,7 @@ $app->register(Valkyrja\Providers\TwigServiceProvider::class);
  */
 
 // Require the routes
-$routes = require_once __DIR__ . '/../routes/routes.php';
+require_once __DIR__ . '/../routes/routes.php';
 
 /*
  *-------------------------------------------------------------------------
