ebdfac8840c914e086273d9df582f0494cf0a762
Melech Mizrachi <melechmizrachi@gmail.com>
2016-12-02 14:09:51 -0800

    Adding client.

---
+++ framework/Contracts/Http/Client.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Valkyrja framework.
+ *
+ * (c) Melech Mizrachi
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Contracts\Http;
+
+/**
+ * Interface Client
+ *
+ * @package Valkyrja\Contracts\Http
+ *
+ * @author Melech Mizrachi
+ */
+interface Client
+{
+    //
+}
---
+++ framework/Http/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Valkyrja framework.
+ *
+ * (c) Melech Mizrachi
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Http;
+
+use Valkyrja\Contracts\Http\Client as ClientContract;
+
+/**
+ * Class Client
+ *
+ * @package Valkyrja\Http
+ *
+ * @author Melech Mizrachi
+ */
+class Client implements ClientContract
+{
+    //
+}
