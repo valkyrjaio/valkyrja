121ec2c18cc691c57ea0c5841dd4d6c2ffd30faa
Melech Mizrachi <melechmizrachi@gmail.com>
2016-12-14 15:59:15 -0800

    Improving if enabled logic.

--- framework/Providers/TwigServiceProvider.php
+++ framework/Providers/TwigServiceProvider.php
@@ -35,55 +35,56 @@ class TwigServiceProvider extends ServiceProvider
      */
     public function publish() : void
     {
-        // Check if twig is enabled in env
-        if ($this->app->isTwigEnabled()) {
-            // Set the env variable for views directory if its not set
-            $this->app->config()->views->twig->dir = $this->app->config()->views->twig->dir ?? Directory::resourcesPath('views/twig');
-
-            /**
-             * Set Twig_Environment instance within container.
-             */
-            $this->app->container()->singleton(
-                Twig_Environment::class,
-                function () {
-                    $loader = new Twig_Loader_Filesystem($this->app->config()->views->twig->dir);
-
-                    $twig = new Twig_Environment(
-                        $loader,
-                        [
-                            'cache'   => $this->app->config()->views->twig->compiledDir,
-                            'debug'   => $this->app->config()->app->debug,
-                            'charset' => 'utf-8',
-                        ]
-                    );
-
-                    $extensions = $this->app->config()->views->twig->extensions ?? [];
-
-                    // Twig Extensions registration
-                    if (is_array($extensions)) {
-                        foreach ($extensions as $extension) {
-                            $twig->addExtension(new $extension());
-                        }
-                    }
-
-                    return $twig;
-                }
-            );
-
-            /**
-             * Reset View instance within container for TwigView.
-             */
-            $this->app->container()->bind(
-                View::class,
-                function ($template = '', array $variables = []) {
-                    $view = new TwigView($this->app, $template, $variables);
-
-                    $view->setTwig($this->app->container()->get(Twig_Environment::class));
-
-                    return $view;
-                }
-            );
+        if (! $this->app->isTwigEnabled()) {
+            return;
         }
+
+        // Set the env variable for views directory if its not set
+        $this->app->config()->views->twig->dir = $this->app->config()->views->twig->dir ?? Directory::resourcesPath('views/twig');
+
+        /**
+         * Set Twig_Environment instance within container.
+         */
+        $this->app->container()->singleton(
+            Twig_Environment::class,
+            function () {
+                $loader = new Twig_Loader_Filesystem($this->app->config()->views->twig->dir);
+
+                $twig = new Twig_Environment(
+                    $loader,
+                    [
+                        'cache'   => $this->app->config()->views->twig->compiledDir,
+                        'debug'   => $this->app->config()->app->debug,
+                        'charset' => 'utf-8',
+                    ]
+                );
+
+                $extensions = $this->app->config()->views->twig->extensions ?? [];
+
+                // Twig Extensions registration
+                if (is_array($extensions)) {
+                    foreach ($extensions as $extension) {
+                        $twig->addExtension(new $extension());
+                    }
+                }
+
+                return $twig;
+            }
+        );
+
+        /**
+         * Reset View instance within container for TwigView.
+         */
+        $this->app->container()->bind(
+            View::class,
+            function ($template = '', array $variables = []) {
+                $view = new TwigView($this->app, $template, $variables);
+
+                $view->setTwig($this->app->container()->get(Twig_Environment::class));
+
+                return $view;
+            }
+        );
     }
 }
  
