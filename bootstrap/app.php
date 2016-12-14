df6f86787a34376cb9e19cd028d542219c1a910f
Melech Mizrachi <melechmizrachi@gmail.com>
2016-12-14 09:56:40 -0800

    Removing copyright code.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,6 @@
 <?php
 
 /*
- * This file is part of the Valkyrja framework.
- *
- * (c) Melech Mizrachi
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-/*
  *-------------------------------------------------------------------------
  * Set The Base Directory
  *-------------------------------------------------------------------------
--- public/index.php
+++ public/index.php
@@ -1,12 +1,10 @@
 <?php
 
-/*
- * This file is part of the Valkyrja framework.
+/**
+ * Valkyrja - PHP framework
  *
- * (c) Melech Mizrachi
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @package Valkyrja
+ * @author  Melech Mizrachi
  */
 
 /*
