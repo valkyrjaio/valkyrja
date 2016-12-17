dbb860e3584344d5b4cb3d3cf1610360082f9beb
Melech Mizrachi <melechmizrachi@gmail.com>
2016-12-16 17:18:13 -0800

    Updating env constants to public to ensure they cannot be set to private by an extended class and break the application.

--- framework/Contracts/Config/Env.php
+++ framework/Contracts/Config/Env.php
@@ -20,31 +20,31 @@ namespace Valkyrja\Contracts\Config;
  */
 interface Env
 {
-    const APP_ENV = null;
-    const APP_DEBUG = null;
-    const APP_URL = null;
-    const APP_TIMEZONE = null;
-    const APP_VERSION = null;
+    public const APP_ENV = null;
+    public const APP_DEBUG = null;
+    public const APP_URL = null;
+    public const APP_TIMEZONE = null;
+    public const APP_VERSION = null;
 
-    const ANNOTATIONS_ENABLED = null;
-    const ANNOTATIONS_CACHE_DIR = null;
+    public const ANNOTATIONS_ENABLED = null;
+    public const ANNOTATIONS_CACHE_DIR = null;
 
-    const ROUTING_TRAILING_SLASH = null;
-    const ROUTING_ALLOW_WITH_TRAILING_SLASH = null;
-    const ROUTING_USE_ANNOTATIONS = null;
-    const ROUTING_USE_ANNOTATIONS_EXCLUSIVELY = null;
-    const ROUTING_CONTROLLERS = null;
-    const ROUTING_ROUTES_FILE = null;
-    const ROUTING_ROUTES_CACHE_FILE = null;
-    const ROUTING_USE_ROUTES_CACHE_FILE = null;
+    public const ROUTING_TRAILING_SLASH = null;
+    public const ROUTING_ALLOW_WITH_TRAILING_SLASH = null;
+    public const ROUTING_USE_ANNOTATIONS = null;
+    public const ROUTING_USE_ANNOTATIONS_EXCLUSIVELY = null;
+    public const ROUTING_CONTROLLERS = null;
+    public const ROUTING_ROUTES_FILE = null;
+    public const ROUTING_ROUTES_CACHE_FILE = null;
+    public const ROUTING_USE_ROUTES_CACHE_FILE = null;
 
-    const STORAGE_UPLOADS_DIR = null;
-    const STORAGE_LOGS_DIR = null;
+    public const STORAGE_UPLOADS_DIR = null;
+    public const STORAGE_LOGS_DIR = null;
 
-    const VIEWS_DIR = null;
+    public const VIEWS_DIR = null;
 
-    const VIEWS_TWIG_ENABLED = null;
-    const VIEWS_TWIG_DIR = null;
-    const VIEWS_TWIG_COMPILED_DIR = null;
-    const VIEWS_TWIG_EXTENSIONS = null;
+    public const VIEWS_TWIG_ENABLED = null;
+    public const VIEWS_TWIG_DIR = null;
+    public const VIEWS_TWIG_COMPILED_DIR = null;
+    public const VIEWS_TWIG_EXTENSIONS = null;
 }
