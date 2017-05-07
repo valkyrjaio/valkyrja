f3bf89102ee8716d04a9d99400c92a1a60a68a2d
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-07 13:17:38 -0700

    Adding php unit testing initial.

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *-------------------------------------------------------------------------
+ * Composer Auto Loader Assemble!
+ *-------------------------------------------------------------------------
+ *
+ * Autoload all the application namespaces, dependencies, and files using
+ * composer to both manage all dependencies as well as register everything
+ * for us to use within the application.
+ *
+ */
+
+require __DIR__.'/../vendor/autoload.php';
