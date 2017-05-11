98e4e60b282e03977ace5c876d936f0eebd5916f
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-10 18:59:11 -0700

    Updating Route model test.

--- tests/Unit/Routing/RouteTest.php
+++ tests/Unit/Routing/RouteTest.php
@@ -3,6 +3,7 @@
 namespace Valkyrja\Tests\Unit\Routing;
 
 use PHPUnit\Framework\TestCase;
+use Valkyrja\Http\RequestMethod;
 use Valkyrja\Routing\Route;
 
 /**
@@ -27,7 +28,7 @@ class RouteTest extends TestCase
     protected $stringValue = 'test';
 
     /**
-     * Test the get path getter method default value.
+     * Test the getPath getter method default value.
      *
      * @return void
      */
@@ -47,22 +48,298 @@ class RouteTest extends TestCase
     }
 
     /**
-     * Test the set path setter method.
+     * Test the setPath setter method.
      *
      * @return void
      */
     public function testSetPath(): void
     {
-        $this->assertEquals(true, $this->getRoute()->setPath($this->stringValue) instanceof Route);
+        $set = $this->getRoute()->setPath($this->stringValue);
+
+        $this->assertEquals(true, $set instanceof Route);
     }
 
     /**
-     * Test the get path getter method.
+     * Test the getPath getter method.
      *
      * @return void
      */
     public function testGetPath(): void
     {
+        $this->getRoute()->setPath($this->stringValue);
+
         $this->assertEquals($this->stringValue, $this->getRoute()->getPath());
+    }
+
+    /**
+     * Test the getRequestMethods getter method default value.
+     *
+     * @return void
+     */
+    public function testGetRequestMethodsDefault(): void
+    {
+        $this->assertEquals(
+            [
+                RequestMethod::GET,
+                RequestMethod::HEAD,
+            ],
+            $this->getRoute()->getRequestMethods()
+        );
+    }
+
+    /**
+     * Test the setRequestMethods setter method.
+     *
+     * @return void
+     */
+    public function testSetRequestMethods(): void
+    {
+        $set = $this->getRoute()->setRequestMethods([RequestMethod::POST]);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the getRequestMethods getter method.
+     *
+     * @return void
+     */
+    public function testGetRequestMethods(): void
+    {
+        $value = [RequestMethod::POST];
+        $this->getRoute()->setRequestMethods($value);
+
+        $this->assertEquals($value, $this->getRoute()->getRequestMethods());
+    }
+
+    /**
+     * Test the getRegex getter method default value.
+     *
+     * @return void
+     */
+    public function testGetRegexDefault(): void
+    {
+        $this->assertEquals(null, $this->getRoute()->getRegex());
+    }
+
+    /**
+     * Test the setRegex setter method.
+     *
+     * @return void
+     */
+    public function testSetRegex(): void
+    {
+        $set = $this->getRoute()->setRegex($this->stringValue);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the setRegex setter method using null.
+     *
+     * @return void
+     */
+    public function testSetRegexNull(): void
+    {
+        $set = $this->getRoute()->setRegex(null);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the getRegex getter method.
+     *
+     * @return void
+     */
+    public function testGetRegex(): void
+    {
+        $this->getRoute()->setRegex($this->stringValue);
+
+        $this->assertEquals($this->stringValue, $this->getRoute()->getRegex());
+    }
+
+    /**
+     * Test the getParams getter method default value.
+     *
+     * @return void
+     */
+    public function testGetParamsDefault(): void
+    {
+        $this->assertEquals(null, $this->getRoute()->getParams());
+    }
+
+    /**
+     * Test the setParams setter method.
+     *
+     * @return void
+     */
+    public function testSetParams(): void
+    {
+        $set = $this->getRoute()->setParams([$this->stringValue]);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the setParams setter method using null.
+     *
+     * @return void
+     */
+    public function testSetParamsNull(): void
+    {
+        $set = $this->getRoute()->setParams(null);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the getParams getter method.
+     *
+     * @return void
+     */
+    public function testGetParams(): void
+    {
+        $this->getRoute()->setParams([$this->stringValue]);
+
+        $this->assertEquals([$this->stringValue], $this->getRoute()->getParams());
+    }
+
+    /**
+     * Test the getSegments getter method default value.
+     *
+     * @return void
+     */
+    public function testGetSegmentsDefault(): void
+    {
+        $this->assertEquals(null, $this->getRoute()->getSegments());
+    }
+
+    /**
+     * Test the setSegments setter method.
+     *
+     * @return void
+     */
+    public function testSetSegments(): void
+    {
+        $set = $this->getRoute()->setSegments([$this->stringValue]);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the setSegments setter method using null.
+     *
+     * @return void
+     */
+    public function testSetSegmentsNull(): void
+    {
+        $set = $this->getRoute()->setSegments(null);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the getSegments getter method.
+     *
+     * @return void
+     */
+    public function testGetSegments(): void
+    {
+        $this->getRoute()->setSegments([$this->stringValue]);
+
+        $this->assertEquals([$this->stringValue], $this->getRoute()->getSegments());
+    }
+
+    /**
+     * Test the getDynamic getter method default value.
+     *
+     * @return void
+     */
+    public function testGetDynamicDefault(): void
+    {
+        $this->assertEquals(false, $this->getRoute()->getDynamic());
+    }
+
+    /**
+     * Test the setDynamic setter method.
+     *
+     * @return void
+     */
+    public function testSetDynamic(): void
+    {
+        $set = $this->getRoute()->setDynamic(true);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the getDynamic getter method.
+     *
+     * @return void
+     */
+    public function testGetDynamic(): void
+    {
+        $this->getRoute()->setDynamic(true);
+
+        $this->assertEquals(true, $this->getRoute()->getDynamic());
+    }
+
+    /**
+     * Test the getSecure getter method default value.
+     *
+     * @return void
+     */
+    public function testGetSecureDefault(): void
+    {
+        $this->assertEquals(false, $this->getRoute()->getSecure());
+    }
+
+    /**
+     * Test the setSecure setter method.
+     *
+     * @return void
+     */
+    public function testSetSecure(): void
+    {
+        $set = $this->getRoute()->setSecure(true);
+
+        $this->assertEquals(true, $set instanceof Route);
+    }
+
+    /**
+     * Test the getSecure getter method.
+     *
+     * @return void
+     */
+    public function testGetSecure(): void
+    {
+        $this->getRoute()->setSecure(true);
+
+        $this->assertEquals(true, $this->getRoute()->getSecure());
+    }
+
+    /**
+     * Test the getRoute method.
+     *
+     * @return void
+     */
+    public function testGetRoute(): void
+    {
+        $route = Route::getRoute(['path' => $this->stringValue]);
+
+        $this->assertEquals(true, $route instanceof Route);
+    }
+
+    /**
+     * Test the __set_state magic method.
+     *
+     * @return void
+     */
+    public function testSetState(): void
+    {
+        /* @noinspection ImplicitMagicMethodCallInspection */
+        $route = Route::__set_state(['path' => $this->stringValue]);
+
+        $this->assertEquals(true, $route instanceof Route);
     }
 }
