3c3b9778f67fb34a60f124fe19c1b5816f36f7d0
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-13 20:48:53 -0700

    Updating AnnotationsParser.

--- src/Valkyrja/Annotations/AnnotationsParser.php
+++ src/Valkyrja/Annotations/AnnotationsParser.php
@@ -166,13 +166,13 @@ class AnnotationsParser implements AnnotationsParserContract
      */
     protected function getAnnotationProperties(array $matches, int $index): array
     {
-        $properties = [];
-
-        $properties['annotation']  = $matches[1][$index] ?? null;
-        $properties['arguments']   = $matches[2][$index] ?? null;
-        $properties['type']        = $matches[3][$index] ?? null;
-        $properties['variable']    = $matches[4][$index] ?? null;
-        $properties['description'] = $matches[5][$index] ?? null;
+        $properties = [
+            'annotation'  => $matches[1][$index] ?? null,
+            'arguments'   => $matches[2][$index] ?? null,
+            'type'        => $matches[3][$index] ?? null,
+            'variable'    => $matches[4][$index] ?? null,
+            'description' => $matches[5][$index] ?? null,
+        ];
 
         return $this->processAnnotationProperties($properties);
     }
