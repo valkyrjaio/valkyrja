8fa3003ff937cd7efbe253c614b65e9be19117bb
Melech Mizrachi <melechmizrachi@gmail.com>
2020-01-30 00:48:03 -0700

    Updating ORM WhereType.php

--- src/Valkyrja/ORM/Enums/WhereType.php
+++ src/Valkyrja/ORM/Enums/WhereType.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 /*
  * This file is part of the Valkyrja framework.
@@ -20,11 +20,35 @@ use Valkyrja\Enum\Enum;
  */
 final class WhereType extends Enum
 {
-    public const EQUALS = '=';
-    public const LIKE   = 'LIKE';
+    public const EQUALS             = '=';
+    public const NOT_EQUAL          = '!=';
+    public const IN                 = 'IN';
+    public const NOT_IN             = 'NOT_IN';
+    public const LIKE               = 'LIKE';
+    public const NOT_LIKE           = 'NOT LIKE';
+    public const IS                 = 'IS';
+    public const IS_NOT             = 'IS NOT';
+    public const GREATER_THAN       = '>';
+    public const GREATER_THAN_EQUAL = '>=';
+    public const LESS_THAN          = '<';
+    public const LESS_THAN_EQUAL    = '<=';
+    public const REGEXP             = 'REGEXP';
+    public const NOT_REGEXP         = 'NOT REGEXP';
 
     protected const VALUES = [
         self::EQUALS,
+        self::NOT_EQUAL,
+        self::IN,
+        self::NOT_IN,
         self::LIKE,
+        self::NOT_LIKE,
+        self::IS,
+        self::IS_NOT,
+        self::GREATER_THAN,
+        self::GREATER_THAN_EQUAL,
+        self::LESS_THAN,
+        self::LESS_THAN_EQUAL,
+        self::REGEXP,
+        self::NOT_REGEXP,
     ];
 }
