fe8200044a938ca591b594895c94fad061f4439e
Melech Mizrachi <melechmizrachi@gmail.com>
2020-02-20 00:36:04 -0700

    Adding container cacheable trait for better reuseability.

--- src/Valkyrja/Container/Cacheables/ContainerCacheable.php
+++ src/Valkyrja/Container/Cacheables/ContainerCacheable.php
@@ -11,7 +11,7 @@ declare(strict_types = 1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Container\Dispatchers;
+namespace Valkyrja\Container\Cacheables;
 
 use Valkyrja\Application\Application;
 use Valkyrja\Config\Enums\ConfigKey;
--- src/Valkyrja/Container/Dispatchers/Container.php
+++ src/Valkyrja/Container/Dispatchers/Container.php
@@ -14,6 +14,7 @@ declare(strict_types = 1);
 namespace Valkyrja\Container\Dispatchers;
 
 use Valkyrja\Application\Application;
+use Valkyrja\Container\Cacheables\ContainerCacheable;
 use Valkyrja\Container\Container as ContainerContract;
 use Valkyrja\Container\Events\ServiceMade;
 use Valkyrja\Container\Events\ServiceMadeSingleton;
