b5f86fb2dc61a95dbf9ee9db3bb20ca1ffde85ed
Melech Mizrachi <melechmizrachi@gmail.com>
2020-02-28 18:52:42 -0700

    Removing comments from tests/bootstrap files.

--- tests/bootstrap/events/default.php
+++ tests/bootstrap/events/default.php
@@ -10,12 +10,3 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-/*
- *-------------------------------------------------------------------------
- * Define Application Event Listeners
- *-------------------------------------------------------------------------
- *
- * TODO: ADD EXPLANATION
- *
- */
