c26668f5c039f483729b34aea67fb9438696df6c
Melech Mizrachi <melechmizrachi@gmail.com>
2020-02-29 21:36:20 -0700

    Refactoring for less function files.

--- functions/helpers.php
+++ functions/helpers.php
@@ -32,6 +32,14 @@ use Valkyrja\Mail\Mail;
 use Valkyrja\ORM\EntityManager;
 use Valkyrja\Session\Session;
 use Valkyrja\View\View;
+use Valkyrja\Http\Exceptions\HttpRedirectException;
+use Valkyrja\Http\JsonResponse;
+use Valkyrja\Http\RedirectResponse;
+use Valkyrja\Http\Request;
+use Valkyrja\Http\ResponseFactory;
+use Valkyrja\Routing\Route;
+use Valkyrja\Routing\Router;
+use Valkyrja\Support\Directory as ValkyrjaDirectory;
 
 if (! function_exists('app')) {
     /**
@@ -311,6 +319,162 @@ if (! function_exists('reflector')) {
     }
 }
 
+if (! function_exists('request')) {
+    /**
+     * Get request.
+     *
+     * @return Request
+     */
+    function request(): Request
+    {
+        return app()->request();
+    }
+}
+
+if (! function_exists('router')) {
+    /**
+     * Get router.
+     *
+     * @return Router
+     */
+    function router(): Router
+    {
+        return app()->router();
+    }
+}
+
+if (! function_exists('route')) {
+    /**
+     * Get a route by name.
+     *
+     * @param string $name The name of the route to get
+     *
+     * @return Route
+     */
+    function route(string $name): Route
+    {
+        return router()->getRoute($name);
+    }
+}
+
+if (! function_exists('routeUrl')) {
+    /**
+     * Get a route url by name.
+     *
+     * @param string $name     The name of the route to get
+     * @param array  $data     [optional] The route data if dynamic
+     * @param bool   $absolute [optional] Whether this url should be absolute
+     *
+     * @return string
+     */
+    function routeUrl(string $name, array $data = null, bool $absolute = null): string
+    {
+        return router()->getUrl($name, $data, $absolute);
+    }
+}
+
+if (! function_exists('responseBuilder')) {
+    /**
+     * Get the response builder.
+     *
+     * @return ResponseFactory
+     */
+    function responseBuilder(): ResponseFactory
+    {
+        return app()->responseFactory();
+    }
+}
+
+if (! function_exists('response')) {
+    /**
+     * Return a new response from the application.
+     *
+     * @param string|null $content    [optional] The content to set
+     * @param int|null    $statusCode [optional] The status code to set
+     * @param array|null  $headers    [optional] The headers to set
+     *
+     * @return Response
+     */
+    function response(string $content = null, int $statusCode = null, array $headers = null): Response
+    {
+        return app()->response($content, $statusCode, $headers);
+    }
+}
+
+if (! function_exists('json')) {
+    /**
+     * Return a new json response from the application.
+     *
+     * @param array|null $data       [optional] An array of data
+     * @param int|null   $statusCode [optional] The status code to set
+     * @param array|null $headers    [optional] The headers to set
+     *
+     * @return JsonResponse
+     */
+    function json(array $data = null, int $statusCode = null, array $headers = null): JsonResponse
+    {
+        return app()->json($data, $statusCode, $headers);
+    }
+}
+
+if (! function_exists('redirect')) {
+    /**
+     * Return a new redirect response from the application.
+     *
+     * @param string|null $uri        [optional] The URI to redirect to
+     * @param int|null    $statusCode [optional] The response status code
+     * @param array|null  $headers    [optional] An array of response headers
+     *
+     * @return RedirectResponse
+     */
+    function redirect(string $uri = null, int $statusCode = null, array $headers = null): RedirectResponse
+    {
+        return app()->redirect($uri, $statusCode, $headers);
+    }
+}
+
+if (! function_exists('redirectRoute')) {
+    /**
+     * Return a new redirect response from the application for a given route.
+     *
+     * @param string     $route      The route to match
+     * @param array|null $parameters [optional] Any parameters to set for dynamic routes
+     * @param int|null   $statusCode [optional] The response status code
+     * @param array|null $headers    [optional] An array of response headers
+     *
+     * @return RedirectResponse
+     */
+    function redirectRoute(
+        string $route,
+        array $parameters = null,
+        int $statusCode = null,
+        array $headers = null
+    ): RedirectResponse {
+        return app()->redirectRoute($route, $parameters, $statusCode, $headers);
+    }
+}
+
+if (! function_exists('redirectTo')) {
+    /**
+     * Redirect to a given uri, and abort the application.
+     *
+     * @param string $uri        [optional] The URI to redirect to
+     * @param int    $statusCode [optional] The response status code
+     * @param array  $headers    [optional] An array of response headers
+     *
+     * @throws HttpRedirectException
+     *
+     * @return void
+     */
+    function redirectTo(
+        string $uri = null,
+        int $statusCode = StatusCode::FOUND,
+        array $headers = []
+    ): void {
+        app()->redirectTo($uri, $statusCode, $headers);
+    }
+}
+
 if (! function_exists('session')) {
     /**
      * Return the session.
@@ -352,5 +516,229 @@ if (! function_exists('dd')) {
         var_dump(func_get_args());
 
         die(1);
+    }
+}
+
+if (! function_exists('basePath')) {
+    /**
+     * Helper function to get base path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function basePath(string $path = null): string
+    {
+        return ValkyrjaDirectory::basePath($path);
+    }
+}
+
+if (! function_exists('appPath')) {
+    /**
+     * Helper function to get app path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function appPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::appPath($path);
+    }
+}
+
+if (! function_exists('bootstrapPath')) {
+    /**
+     * Helper function to get bootstrap path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function bootstrapPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::bootstrapPath($path);
+    }
+}
+
+if (! function_exists('envPath')) {
+    /**
+     * Helper function to get env path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function envPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::envPath($path);
+    }
+}
+
+if (! function_exists('configPath')) {
+    /**
+     * Helper function to get config path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function configPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::configPath($path);
+    }
+}
+
+if (! function_exists('commandsPath')) {
+    /**
+     * Helper function to get commands path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function commandsPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::commandsPath($path);
+    }
+}
+
+if (! function_exists('eventsPath')) {
+    /**
+     * Helper function to get events path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function eventsPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::eventsPath($path);
+    }
+}
+
+if (! function_exists('routesPath')) {
+    /**
+     * Helper function to get routes path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function routesPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::routesPath($path);
+    }
+}
+
+if (! function_exists('servicesPath')) {
+    /**
+     * Helper function to get services path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function servicesPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::servicesPath($path);
+    }
+}
+
+if (! function_exists('publicPath')) {
+    /**
+     * Helper function to get public path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function publicPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::publicPath($path);
+    }
+}
+
+if (! function_exists('resourcesPath')) {
+    /**
+     * Helper function to get resources path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function resourcesPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::resourcesPath($path);
+    }
+}
+
+if (! function_exists('storagePath')) {
+    /**
+     * Helper function to get storage path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function storagePath(string $path = null): string
+    {
+        return ValkyrjaDirectory::storagePath($path);
+    }
+}
+
+if (! function_exists('frameworkStoragePath')) {
+    /**
+     * Helper function to get framework storage path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function frameworkStoragePath(string $path = null): string
+    {
+        return ValkyrjaDirectory::frameworkStoragePath($path);
+    }
+}
+
+if (! function_exists('cachePath')) {
+    /**
+     * Helper function to get cache path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function cachePath(string $path = null): string
+    {
+        return ValkyrjaDirectory::cachePath($path);
+    }
+}
+
+if (! function_exists('testsPath')) {
+    /**
+     * Helper function to get tests path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function testsPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::testsPath($path);
+    }
+}
+
+if (! function_exists('vendorPath')) {
+    /**
+     * Helper function to get vendor path.
+     *
+     * @param string $path [optional] The path to append
+     *
+     * @return string
+     */
+    function vendorPath(string $path = null): string
+    {
+        return ValkyrjaDirectory::vendorPath($path);
     }
 }
