5251e0e47bfe9dd33746aa4734975e3ffb133be8
Melech Mizrachi <melechmizrachi@gmail.com>
2020-03-24 18:51:08 -0700

    Updating Middleware so as to not have to implement all methods.

--- src/Valkyrja/Http/Middleware/Middleware.php
+++ src/Valkyrja/Http/Middleware/Middleware.php
@@ -31,7 +31,12 @@ abstract class Middleware implements MiddlewareContract
      *
      * @return Request|Response
      */
-    abstract public static function before(Request $request);
+    public static function before(Request $request)
+    {
+        // Do logic using the request before it is processed by the controller action, here
+
+        return $request;
+    }
 
     /**
      * Middleware handler for after a request is dispatched.
@@ -41,7 +46,12 @@ abstract class Middleware implements MiddlewareContract
      *
      * @return Response
      */
-    abstract public static function after(Request $request, Response $response): Response;
+    public static function after(Request $request, Response $response): Response
+    {
+        // Modify the response after its been processed by the controller action, here
+
+        return $response;
+    }
 
     /**
      * Middleware handler run when the application is terminating.
@@ -51,5 +61,8 @@ abstract class Middleware implements MiddlewareContract
      *
      * @return void
      */
-    abstract public static function terminate(Request $request, Response $response): void;
+    public static function terminate(Request $request, Response $response): void
+    {
+        // Do stuff after termination (after the response has been sent) here
+    }
 }
