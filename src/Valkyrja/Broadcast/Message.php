f80ca466b9e479beb2ea04b252588938d187a965
Melech Mizrachi <melechmizrachi@gmail.com>
2020-07-20 21:41:25 -0700

    Adding missing methods.

--- src/Valkyrja/Broadcast/Message.php
+++ src/Valkyrja/Broadcast/Message.php
@@ -20,19 +20,67 @@ namespace Valkyrja\Broadcast;
  */
 interface Message
 {
+    /**
+     * Get the channel to broadcast on.
+     *
+     * @return string
+     */
     public function getChannel(): string;
 
+    /**
+     * Set the channel to broadcast on.
+     *
+     * @param string $channel The channel
+     *
+     * @return static
+     */
     public function setChannel(string $channel): self;
 
+    /**
+     * Get the event to broadcast.
+     *
+     * @return string
+     */
     public function getEvent(): string;
 
+    /**
+     * Set the event to broadcast.
+     *
+     * @param string $event The event
+     *
+     * @return static
+     */
     public function setEvent(string $event): self;
 
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array|null
+     */
     public function getData(): ?array;
 
+    /**
+     * Set the data to broadcast.
+     *
+     * @param array|null $data [optional] The data
+     *
+     * @return static
+     */
     public function setData(array $data = null): self;
 
+    /**
+     * Get the message to broadcast.
+     *
+     * @return string
+     */
     public function getMessage(): string;
 
+    /**
+     * Set the message to broadcast.
+     *
+     * @param string $message The message
+     *
+     * @return static
+     */
     public function setMessage(string $message): self;
 }
--- src/Valkyrja/Broadcast/Messages/Message.php
+++ src/Valkyrja/Broadcast/Messages/Message.php
@@ -22,4 +22,127 @@ use Valkyrja\Broadcast\Message as Contract;
  */
 class Message implements Contract
 {
+    /**
+     * The channel to broadcast to.
+     *
+     * @var string
+     */
+    protected string $channel;
+
+    /**
+     * The event to broadcast to.
+     *
+     * @var string
+     */
+    protected string $event;
+
+    /**
+     * The message to broadcast.
+     *
+     * @var string
+     */
+    protected string $message;
+
+    /**
+     * The data to broadcast.
+     *
+     * @var array|null
+     */
+    protected ?array $data = null;
+
+    /**
+     * Get the channel to broadcast on.
+     *
+     * @return string
+     */
+    public function getChannel(): string
+    {
+        return $this->channel;
+    }
+
+    /**
+     * Set the channel to broadcast on.
+     *
+     * @param string $channel The channel
+     *
+     * @return static
+     */
+    public function setChannel(string $channel): self
+    {
+        $this->channel = $channel;
+
+        return $this;
+    }
+
+    /**
+     * Get the event to broadcast.
+     *
+     * @return string
+     */
+    public function getEvent(): string
+    {
+        return $this->event;
+    }
+
+    /**
+     * Set the event to broadcast.
+     *
+     * @param string $event The event
+     *
+     * @return static
+     */
+    public function setEvent(string $event): self
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array|null
+     */
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set the data to broadcast.
+     *
+     * @param array|null $data [optional] The data
+     *
+     * @return static
+     */
+    public function setData(array $data = null): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get the message to broadcast.
+     *
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set the message to broadcast.
+     *
+     * @param string $message The message
+     *
+     * @return static
+     */
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
 }
