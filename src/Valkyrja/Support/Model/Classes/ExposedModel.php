b0410d1c6091f1665f6372e94a1a80a252d12398
Melech Mizrachi <melechmizrachi@gmail.com>
2022-11-26 17:35:46 -0700

    Routing: Add ExposedModel that would expose all properties, regardless of visibility.

---
+++ src/Valkyrja/Support/Model/Classes/ExposedModel.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Support\Model\Classes;
+
+use Valkyrja\Support\Model\Traits\ExposedModelTrait;
+
+/**
+ * Class ExposedModel.
+ *
+ * @author Melech Mizrachi
+ */
+abstract class ExposedModel extends Model
+{
+    use ExposedModelTrait;
+}
---
+++ src/Valkyrja/Support/Model/Traits/ExposedModelTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Support\Model\Traits;
+
+/**
+ * Trait ExposedModelTrait.
+ *
+ * @author Melech Mizrachi
+ */
+trait ExposedModelTrait
+{
+    /**
+     * @inheritDoc
+     */
+    protected function __allProperties(bool $includeHidden = false): array
+    {
+        return get_object_vars($this);
+    }
+}
