1b25226e5d429b5bf4e6cddaa84cf7e9cb12c6ab
Melech Mizrachi <melechmizrachi@gmail.com>
2022-12-05 18:09:59 -0700

    Container: Formatting change to Container config.

--- src/Valkyrja/Container/Config/Container.php
+++ src/Valkyrja/Container/Config/Container.php
@@ -31,10 +31,7 @@ class Container extends Model
     {
         $this->updateProperties(ConfigValue::$defaults);
 
-        $this->providers     = array_merge(
-            ConfigValue::PROVIDERS,
-            []
-        );
+        $this->providers     = array_merge(ConfigValue::PROVIDERS, []);
         $this->devProviders  = array_merge(ConfigValue::DEV_PROVIDERS, []);
         $this->filePath      = servicesPath('default.php');
         $this->cacheFilePath = cachePath('container.php');
