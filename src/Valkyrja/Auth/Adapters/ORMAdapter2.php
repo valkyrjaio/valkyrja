6be9a9fc4fc914f13445a0b870f69bf6d239a15f
Melech Mizrachi <melechmizrachi@gmail.com>
2024-02-27 18:36:59 -0700

    Auth: New Adapter paradigm.

---
+++ src/Valkyrja/Auth/Adapters/ORMAdapter2.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Auth\Adapters;
+
+use Valkyrja\Auth\AuthenticationAttempt;
+use Valkyrja\Auth\AuthenticationRetrieval;
+use Valkyrja\Auth\Config\Config;
+use Valkyrja\Auth\User;
+use Valkyrja\Orm\Orm;
+
+/**
+ * Class ORMAdapter.
+ *
+ * @author Melech Mizrachi
+ */
+abstract class ORMAdapter2 extends Adapter2
+{
+    /**
+     * ORMAdapter constructor.
+     *
+     * @param class-string<User> $user
+     */
+    public function __construct(
+        protected Orm $orm,
+        string $user,
+        Config|array $config
+    ) {
+        parent::__construct($user, $config);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function authenticate(AuthenticationAttempt $attempt): User|null
+    {
+        // TODO: Implement authenticate() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function retrieve(AuthenticationRetrieval $retrieval): User|null
+    {
+        // TODO: Implement retrieve() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(User $user): bool
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function save(User $user): void
+    {
+        // TODO: Implement save() method.
+    }
+}
---
+++ src/Valkyrja/Auth/Models/AuthenticationRetrieval.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Auth\Models;
+
+use Valkyrja\Auth\AuthenticationRetrieval as Contract;
+use Valkyrja\Auth\User;
+
+/**
+ * Class AuthenticationRetrieval.
+ *
+ * @author Melech Mizrachi
+ */
+class AuthenticationRetrieval implements Contract
+{
+    public function __construct(
+        protected string $username,
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param class-string<User> $user
+     */
+    public function getRetrievalFields(string $user): array
+    {
+        return [
+            $user::getUsernameField() => $this->username,
+        ];
+    }
+}
