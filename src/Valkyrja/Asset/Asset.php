cdc15b095eb24dd13a2775e35e0bb0c09d182983
Melech Mizrachi <melechmizrachi@gmail.com>
2024-05-21 19:16:38 -0700

    Asset: Updating module namespaces.

--- src/Valkyrja/Asset/Asset.php
+++ src/Valkyrja/Asset/Asset.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Asset\Managers;
+namespace Valkyrja\Asset;
 
 use Valkyrja\Asset\Adapter\Contract\Adapter;
 use Valkyrja\Asset\Contract\Asset as Contract;
