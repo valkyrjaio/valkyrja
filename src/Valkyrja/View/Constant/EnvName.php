4b1ea5bed16a1579e7053a964e2e30e8067767b7
Melech Mizrachi <melechmizrachi@gmail.com>
2025-06-15 00:34:18 -0700

    View: Adding missing env names.

--- src/Valkyrja/View/Constant/EnvName.php
+++ src/Valkyrja/View/Constant/EnvName.php
@@ -20,8 +20,5 @@ namespace Valkyrja\View\Constant;
  */
 final class EnvName
 {
-    public const DIR                   = 'VIEW_DIR';
-    public const PATH                  = 'VIEW_PATH';
-    public const DEFAULT_CONFIGURATION = 'VIEW_DEFAULT_CONFIGURATION';
-    public const CONFIGURATIONS        = 'VIEW_CONFIGURATIONS';
+    public const string DEFAULT_CONFIGURATION = 'VIEW_DEFAULT_CONFIGURATION';
 }
