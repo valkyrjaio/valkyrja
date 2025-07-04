e592845d85e7fad1e0587986ee08c0eb4897562a
Melech Mizrachi <melechmizrachi@gmail.com>
2025-07-03 20:21:19 -0700

    PHP CS Fixer: Fixing issues.

--- src/Valkyrja/Jwt/Provider/ServiceProvider.php
+++ src/Valkyrja/Jwt/Provider/ServiceProvider.php
@@ -82,16 +82,16 @@ final class ServiceProvider extends Provider
         $encodeKey = match ($algorithm) {
             Algorithm::HS256, Algorithm::HS384, Algorithm::HS512 => $env::JWT_HS_KEY,
             Algorithm::RS256, Algorithm::RS384, Algorithm::RS512 => $env::JWT_RS_PRIVATE_KEY,
-            Algorithm::EdDSA                                     => $env::JWT_EDDSA_PRIVATE_KEY,
-            default                                              => $env::APP_KEY,
+            Algorithm::EdDSA => $env::JWT_EDDSA_PRIVATE_KEY,
+            default          => $env::APP_KEY,
         };
 
         /** @var OpenSSLAsymmetricKey|OpenSSLCertificate|string $decodeKey */
         $decodeKey = match ($algorithm) {
             Algorithm::HS256, Algorithm::HS384, Algorithm::HS512 => $env::JWT_HS_KEY,
             Algorithm::RS256, Algorithm::RS384, Algorithm::RS512 => $env::JWT_RS_PUBLIC_KEY,
-            Algorithm::EdDSA                                     => $env::JWT_EDDSA_PUBLIC_KEY,
-            default                                              => $env::APP_KEY,
+            Algorithm::EdDSA => $env::JWT_EDDSA_PUBLIC_KEY,
+            default          => $env::APP_KEY,
         };
 
         $container->setSingleton(
