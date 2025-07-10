718fb63d2e2221834bfec10c0e94b043b6bb90e9
Melech Mizrachi <melechmizrachi@gmail.com>
2025-07-09 20:48:07 -0700

    Http Routing: Updating ListCommand.

--- src/Valkyrja/Http/Routing/Command/ListCommand.php
+++ src/Valkyrja/Http/Routing/Command/ListCommand.php
@@ -14,15 +14,19 @@ declare(strict_types=1);
 namespace Valkyrja\Http\Routing\Command;
 
 use Valkyrja\Cli\Interaction\Enum\ExitCode;
+use Valkyrja\Cli\Interaction\Enum\TextColor;
 use Valkyrja\Cli\Interaction\Factory\Contract\OutputFactory;
+use Valkyrja\Cli\Interaction\Formatter\Formatter;
+use Valkyrja\Cli\Interaction\Formatter\HighlightedTextFormatter;
 use Valkyrja\Cli\Interaction\Message\Banner;
 use Valkyrja\Cli\Interaction\Message\ErrorMessage;
 use Valkyrja\Cli\Interaction\Message\Message;
-use Valkyrja\Cli\Interaction\Message\SuccessMessage;
+use Valkyrja\Cli\Interaction\Message\NewLine;
 use Valkyrja\Cli\Interaction\Output\Contract\Output;
 use Valkyrja\Cli\Routing\Attribute\Command as CommandAttribute;
-use Valkyrja\Cli\Routing\Data\Contract\Command;
+use Valkyrja\Cli\Routing\Command\VersionCommand;
 use Valkyrja\Http\Routing\Collection\Contract\Collection;
+use Valkyrja\Http\Routing\Data\Contract\Route;
 
 /**
  * Class ListCommand.
@@ -38,7 +42,7 @@ class ListCommand
         description: 'List all routes',
         helpText: new Message('A command to list all the routes present within the Http component.'),
     )]
-    public function run(Command $command, Collection $collection, OutputFactory $outputFactory): Output
+    public function run(VersionCommand $version, Collection $collection, OutputFactory $outputFactory): Output
     {
         $output = $outputFactory
             ->createOutput();
@@ -53,9 +57,32 @@ class ListCommand
                 );
         }
 
-        return $output
+        usort($routes, static fn (Route $a, Route $b): int => $a->getPath() <=> $b->getPath());
+
+        $output = $version
+            ->run($outputFactory)
             ->withAddedMessages(
-                new Banner(new SuccessMessage('Routes list!')),
+                new NewLine(),
+                new Message('Routes:', new HighlightedTextFormatter()),
+                new NewLine()
             );
+
+        foreach ($routes as $item) {
+            $output = $output->withAddedMessages(
+                new Message('  '),
+                new Message($item->getPath(), new Formatter(textColor: TextColor::MAGENTA)),
+                new NewLine(),
+                new Message('    - '),
+                new Message('Name: '),
+                new Message($item->getName(), new HighlightedTextFormatter()),
+                new NewLine(),
+                new Message('    - '),
+                new Message('Dispatch: '),
+                new Message($item->getDispatch()->__toString(), new HighlightedTextFormatter()),
+                new NewLine(),
+            );
+        }
+
+        return $output;
     }
 }
