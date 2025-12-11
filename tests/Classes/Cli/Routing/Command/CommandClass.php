386324f89cdb6648002111933cc7fbdecb0f3889
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-10 23:06:35 -0700

    Cli: Adding more routing related tests.

---
+++ tests/Classes/Cli/Routing/Command/CommandClass.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Tests\Classes\Cli\Routing\Command;
+
+use Valkyrja\Cli\Interaction\Factory\Contract\OutputFactory;
+use Valkyrja\Cli\Interaction\Message\Message;
+use Valkyrja\Cli\Interaction\Output\Contract\Output;
+use Valkyrja\Cli\Routing\Attribute\Command as CommandAttribute;
+use Valkyrja\Cli\Routing\Data\OptionParameter;
+use Valkyrja\Cli\Routing\Enum\OptionMode;
+
+/**
+ * Command class to test commands.
+ *
+ * @author Melech Mizrachi
+ */
+class CommandClass
+{
+    /** @var non-empty-string */
+    public const string NAME = 'test';
+    /** @var non-empty-string */
+    public const string DESCRIPTION = 'A test command';
+    /** @var non-empty-string */
+    public const string HELP_TEXT = 'A test command';
+
+    #[CommandAttribute(
+        name: self::NAME,
+        description: self::DESCRIPTION,
+        helpText: new Message(self::HELP_TEXT),
+        parameters: [
+            new OptionParameter(
+                name: 'command',
+                description: 'The name of the command to get help for',
+                valueDisplayName: 'command',
+                mode: OptionMode::REQUIRED
+            ),
+        ]
+    )]
+    public function run(OutputFactory $outputFactory): Output
+    {
+
+    }
+}
---
+++ tests/Unit/Cli/Routing/Collector/AttributeCollectorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\Tests\Unit\Cli\Routing\Collector;
+
+use ReflectionException;
+use Valkyrja\Attribute\Attributes;
+use Valkyrja\Cli\Routing\Collector\AttributeCollector;
+use Valkyrja\Reflection\Reflection;
+use Valkyrja\Tests\Classes\Cli\Routing\Command\CommandClass;
+use Valkyrja\Tests\Unit\TestCase;
+
+/**
+ * Test the AttributeCollector class.
+ *
+ * @author Melech Mizrachi
+ */
+class AttributeCollectorTest extends TestCase
+{
+    /**
+     * @throws ReflectionException
+     */
+    public function testDefaults(): void
+    {
+        $collector = new AttributeCollector(
+            attributes: new Attributes(),
+            reflection: new Reflection()
+        );
+
+        self::assertEmpty($collector->getCommands(self::class));
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testGetCommands(): void
+    {
+        $collector = new AttributeCollector(
+            attributes: new Attributes(),
+            reflection: new Reflection()
+        );
+
+        $commands = $collector->getCommands(CommandClass::class);
+
+        self::assertNotEmpty($commands);
+        self::assertCount(1, $commands);
+    }
+}
