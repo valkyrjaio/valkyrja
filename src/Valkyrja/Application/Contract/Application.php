ad5e0221d22cba8beda4d2b1ca4b07a18cb65206
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2025-12-14 01:21:54 +0000

    [Release] Update version for v25.2.14 release

--- src/Valkyrja/Application/Contract/Application.php
+++ src/Valkyrja/Application/Contract/Application.php
@@ -31,7 +31,7 @@ interface Application
      *
      * @var non-empty-string
      */
-    public const string VERSION = '25.2.13';
+    public const string VERSION = '25.2.14';
 
     /**
      * The valkyrja framework ascii art.
