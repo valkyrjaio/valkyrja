979146e49dadd9f658fc99cad60b15940641984a
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-28 15:34:50 -0700

    [Api] Moving Api classes to a new Manager namespace for better organization.

--- src/Valkyrja/Api/Manager/Contract/Api.php
+++ src/Valkyrja/Api/Manager/Contract/Api.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Api\Contract;
+namespace Valkyrja\Api\Manager\Contract;
 
 use Throwable;
 use Valkyrja\Api\Model\Contract\Json;
