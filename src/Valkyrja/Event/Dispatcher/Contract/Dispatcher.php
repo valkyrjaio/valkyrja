8133d6ccac2d578a4d243603fcfa7126e9709d74
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-28 14:32:14 -0700

    [Event] Moving Dispatcher classes to a new Dispatcher namespace for better organization.

--- src/Valkyrja/Event/Dispatcher/Contract/Dispatcher.php
+++ src/Valkyrja/Event/Dispatcher/Contract/Dispatcher.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Event\Contract;
+namespace Valkyrja\Event\Dispatcher\Contract;
 
 use Override;
 use Psr\EventDispatcher\EventDispatcherInterface;
