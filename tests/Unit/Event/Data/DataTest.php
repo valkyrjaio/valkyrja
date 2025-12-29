d0ea39c90342d75a8be98229aa0cd5525e852a4e
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-28 20:21:30 -0700

    [Event] Move data test to data namespace.

--- tests/Unit/Event/Data/DataTest.php
+++ tests/Unit/Event/Data/DataTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Tests\Unit\Event;
+namespace Valkyrja\Tests\Unit\Event\Data;
 
 use Valkyrja\Event\Data\Data;
 use Valkyrja\Tests\Unit\TestCase;
