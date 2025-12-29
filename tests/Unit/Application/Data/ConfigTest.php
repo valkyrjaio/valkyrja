d5653425f374c0e32432c7576148757d259fb4cb
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-28 20:16:54 -0700

    [Application] Move tests to appropriate namespace to match main component structure.

--- tests/Unit/Application/Data/ConfigTest.php
+++ tests/Unit/Application/Data/ConfigTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Tests\Unit\Application;
+namespace Valkyrja\Tests\Unit\Application\Data;
 
 use Valkyrja\Application\Data\Config;
 use Valkyrja\Tests\Unit\TestCase;
