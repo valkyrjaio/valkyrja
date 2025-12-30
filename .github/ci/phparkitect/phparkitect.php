03301c185616b31da254ecc91e176bb09002d4c8
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-30 11:01:12 -0700

    [CI] Add new phparkitect rule for enum naming (#281)
    # Description
    
    Add new phparkitect rule for enum naming.
    
    ## Types of changes
    
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
        <!-- Target the lowest major affected branch -->
    - [X] New feature _(non-breaking change which adds functionality)_
        <!-- Target master -->
    - [ ] Deprecation _(breaking change which removes functionality)_
        <!-- Target master -->
    - [ ] Breaking change _(fix or feature that would cause existing
    functionality
      to change)_
    <!-- Target master, unless this is a bug fix in which case let's chat
    -->
    - [ ] Documentation improvement
        <!-- Target appropriate branch -->

--- .github/ci/phparkitect/phparkitect.php
+++ .github/ci/phparkitect/phparkitect.php
@@ -209,6 +209,18 @@ return static function (Config $config): void {
                       ->should(new NotResideInTheseNamespaces('*Enum\\'))
                       ->because('All non-enums should be in an appropriate namespace');
 
+    $srcRules[] = Rule::allClasses()
+                      ->that(new IsEnum())
+                      ->should(new NotHaveNameMatching('*Enum*'))
+                      ->because('All enums should be appropriately named');
+
+    $srcRules[] = Rule::allClasses()
+                      ->that(new IsNotEnum())
+                      ->andThat(new NotResideInTheseNamespaces('Valkyrja\\Type\\BuiltIn\\Enum'))
+                      ->andThat(new NotHaveNameMatching('EnhancedEnumSupport'))
+                      ->should(new NotHaveNameMatching('*Enum*'))
+                      ->because('All non-enums should not be named with Enum');
+
     $testRules[] = Rule::allClasses()
                        ->that(new ResideInOneOfTheseNamespaces('*Classes\\'))
                        ->andThat(new NotHaveNameMatching('*Enum'))
