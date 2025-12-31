681bc25f9dab859fd4db2a87d36bb632c3f41d4b
Melech Mizrachi <melechmizrachi@gmail.com>
2025-12-30 20:34:33 -0700

    [Test] Move tests to the namespace that corresponds with the main component structure.

--- tests/Unit/Type/Abstract/TypeTest.php
+++ tests/Unit/Type/Abstract/TypeTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Valkyrja\Tests\Unit\Type;
+namespace Valkyrja\Tests\Unit\Type\Abstract;
 
 use JsonSerializable;
 use Valkyrja\Tests\Classes\Type\TypeClass;
