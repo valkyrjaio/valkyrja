2fd0f352a4825f51df86f58b0949ae5cbf0fd3cc
Melech Mizrachi <melechmizrachi@gmail.com>
2026-02-05 12:41:54 -0700

    [View] Refactor the ServiceProvider::publishOrkaRenderer method.

--- src/Valkyrja/View/Provider/ServiceProvider.php
+++ src/Valkyrja/View/Provider/ServiceProvider.php
@@ -124,60 +124,8 @@ final class ServiceProvider extends Provider
         /** @var array<non-empty-string, non-empty-string> $paths */
         $paths = $env::VIEW_ORKA_PATHS
             ?? [];
-        /** @var class-string<ReplacementContract>[] $coreReplacements */
-        $coreReplacements = $env::VIEW_ORKA_CORE_REPLACEMENTS
-            ?? [
-                OrkaReplacement::LAYOUT,
-                OrkaReplacement::BLOCK,
-                OrkaReplacement::END_BLOCK,
-                OrkaReplacement::START_BLOCK,
-                OrkaReplacement::TRIM_BLOCK,
-                OrkaReplacement::END_MULTILINE_COMMENT,
-                OrkaReplacement::SINGLE_LINE_COMMENT,
-                OrkaReplacement::START_MULTILINE_COMMENT,
-                OrkaReplacement::PARTIAL,
-                OrkaReplacement::PARTIAL_WITH_VARIABLES,
-                OrkaReplacement::TRIM_PARTIAL,
-                OrkaReplacement::TRIM_PARTIAL_WITH_VARIABLES,
-                OrkaReplacement::BREAK_,
-                OrkaReplacement::ELSE_HAS_BLOCK,
-                OrkaReplacement::HAS_BLOCK,
-                OrkaReplacement::UNLESS_BLOCK,
-                OrkaReplacement::ELSE_,
-                OrkaReplacement::ELSE_IF,
-                OrkaReplacement::ELSE_UNLESS,
-                OrkaReplacement::EMPTY_,
-                OrkaReplacement::END_IF,
-                OrkaReplacement::IF_,
-                OrkaReplacement::ISSET_,
-                OrkaReplacement::NOT_EMPTY,
-                OrkaReplacement::UNLESS,
-                OrkaReplacement::END_FOR,
-                OrkaReplacement::END_FOREACH,
-                OrkaReplacement::FOR_,
-                OrkaReplacement::FOREACH_,
-                OrkaReplacement::CASE_,
-                OrkaReplacement::DEFAULT_,
-                OrkaReplacement::END_SWITCH,
-                OrkaReplacement::SWITCH_,
-                OrkaReplacement::ESCAPED,
-                OrkaReplacement::SET_VARIABLE,
-                OrkaReplacement::SET_VARIABLES,
-                OrkaReplacement::UNESCAPED,
-            ];
-        /** @var class-string<ReplacementContract>[] $replacements */
-        $replacements = $env::VIEW_ORKA_REPLACEMENTS
-            ?? [
-                OrkaReplacement::DEBUG,
-            ];
 
-        $allReplacements = array_merge($coreReplacements, $replacements);
-
-        $replacementClasses = [];
-
-        foreach ($allReplacements as $replacement) {
-            $replacementClasses[] = $container->get($replacement);
-        }
+        $replacementClasses = self::getOrkaComponents($container, $env);
 
         $container->setSingleton(
             OrkaRenderer::class,
@@ -267,5 +215,70 @@ final class ServiceProvider extends Provider
                 $container->getSingleton(RendererContract::class)
             )
         );
+    }
+
+    /**
+     * Get the orka components.
+     *
+     * @return ReplacementContract[]
+     */
+    protected static function getOrkaComponents(ContainerContract $container, Env $env): array
+    {
+        /** @var class-string<ReplacementContract>[] $coreReplacements */
+        $coreReplacements = $env::VIEW_ORKA_CORE_REPLACEMENTS
+            ?? [
+                OrkaReplacement::LAYOUT,
+                OrkaReplacement::BLOCK,
+                OrkaReplacement::END_BLOCK,
+                OrkaReplacement::START_BLOCK,
+                OrkaReplacement::TRIM_BLOCK,
+                OrkaReplacement::END_MULTILINE_COMMENT,
+                OrkaReplacement::SINGLE_LINE_COMMENT,
+                OrkaReplacement::START_MULTILINE_COMMENT,
+                OrkaReplacement::PARTIAL,
+                OrkaReplacement::PARTIAL_WITH_VARIABLES,
+                OrkaReplacement::TRIM_PARTIAL,
+                OrkaReplacement::TRIM_PARTIAL_WITH_VARIABLES,
+                OrkaReplacement::BREAK_,
+                OrkaReplacement::ELSE_HAS_BLOCK,
+                OrkaReplacement::HAS_BLOCK,
+                OrkaReplacement::UNLESS_BLOCK,
+                OrkaReplacement::ELSE_,
+                OrkaReplacement::ELSE_IF,
+                OrkaReplacement::ELSE_UNLESS,
+                OrkaReplacement::EMPTY_,
+                OrkaReplacement::END_IF,
+                OrkaReplacement::IF_,
+                OrkaReplacement::ISSET_,
+                OrkaReplacement::NOT_EMPTY,
+                OrkaReplacement::UNLESS,
+                OrkaReplacement::END_FOR,
+                OrkaReplacement::END_FOREACH,
+                OrkaReplacement::FOR_,
+                OrkaReplacement::FOREACH_,
+                OrkaReplacement::CASE_,
+                OrkaReplacement::DEFAULT_,
+                OrkaReplacement::END_SWITCH,
+                OrkaReplacement::SWITCH_,
+                OrkaReplacement::ESCAPED,
+                OrkaReplacement::SET_VARIABLE,
+                OrkaReplacement::SET_VARIABLES,
+                OrkaReplacement::UNESCAPED,
+            ];
+        /** @var class-string<ReplacementContract>[] $replacements */
+        $replacements = $env::VIEW_ORKA_REPLACEMENTS
+            ?? [
+                OrkaReplacement::DEBUG,
+            ];
+
+        $allReplacements = array_merge($coreReplacements, $replacements);
+
+        $replacementClasses = [];
+
+        foreach ($allReplacements as $replacement) {
+            $replacementClasses[] = $container->get($replacement);
+        }
+
+        return $replacementClasses;
     }
 }
