4fe401c71aa99c7af2d10efe485babf337532c0f
Melech Mizrachi <melechmizrachi@gmail.com>
2026-02-04 20:11:52 -0700

    [Cli] Refactor the OptionFactory class (#546)
    # Description
    
    Refactor the OptionFactory class.
    
    ## Types of changes
    
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
        <!-- Target the lowest major affected branch -->
    - [X] New feature _(non-breaking change which adds functionality)_
        <!-- Target master -->
    - [ ] Deprecation _(breaking change which removes functionality)_
        <!-- Target master -->
    - [ ] Breaking change _(fix or feature that would cause existing
    functionality
      to change)_
    <!-- Target master, unless this is a bug fix in which case let's chat
    -->
    - [ ] Documentation improvement
        <!-- Target appropriate branch -->

--- src/Valkyrja/Cli/Interaction/Option/Factory/OptionFactory.php
+++ src/Valkyrja/Cli/Interaction/Option/Factory/OptionFactory.php
@@ -28,13 +28,9 @@ abstract class OptionFactory
      */
     public static function fromArg(string $arg): array
     {
-        if (! str_starts_with($arg, '-')) {
-            throw new InvalidArgumentException('Options must begin with either a `-` or `--`');
-        }
+        self::validateArgIsOption($arg);
 
-        $type = str_starts_with($arg, '--')
-            ? OptionType::LONG
-            : OptionType::SHORT;
+        $type = self::getOptionType($arg);
 
         $parts = explode('=', $arg);
         $name  = trim($parts[0], '- ');
@@ -44,25 +40,15 @@ abstract class OptionFactory
             $value = null;
         }
 
+        self::validateNonEmptyName($name);
+
+        // Finds short options combined together
+        // e.g. -abc instead of -a -b -c
         if ($type === OptionType::SHORT && strlen($name) > 1) {
-            if ($value !== null) {
-                throw new InvalidArgumentException('Cannot combine multiple options and include a value');
-            }
+            self::validateValueIsEmpty($value);
 
-            $options = [];
-
-            foreach (str_split($name) as $item) {
-                /** @var non-empty-string $item */
-                $options[] = new Option(
-                    name: $item,
-                    type: $type
-                );
-            }
-
-            return $options;
+            return self::splitCombinedShortOptions($type, $name);
         }
-
-        /** @var non-empty-string $name */
 
         return [
             new Option(
@@ -71,5 +57,78 @@ abstract class OptionFactory
                 type: $type
             ),
         ];
+    }
+
+    /**
+     * Validate that an arg is an option.
+     *
+     * @param non-empty-string $arg The arg
+     */
+    protected static function validateArgIsOption(string $arg): void
+    {
+        if (! str_starts_with($arg, '-')) {
+            throw new InvalidArgumentException('Options must begin with either a `-` or `--`');
+        }
+    }
+
+    /**
+     * Validate that an option name is not empty.
+     *
+     * @psalm-assert non-empty-string $name
+     *
+     * @phpstan-assert non-empty-string $name
+     */
+    protected static function validateNonEmptyName(string $name): void
+    {
+        if ($name === '') {
+            throw new InvalidArgumentException('Option name cannot be empty');
+        }
+    }
+
+    /**
+     * Get the type of option based on the name prefix.
+     *
+     * @param non-empty-string $arg The arg
+     */
+    protected static function getOptionType(string $arg): OptionType
+    {
+        return str_starts_with($arg, '--')
+            ? OptionType::LONG
+            : OptionType::SHORT;
+    }
+
+    /**
+     * Validate that a value is not provided when multiple options are provided.
+     *
+     * @param string|null $value The value
+     */
+    protected static function validateValueIsEmpty(string|null $value = null): void
+    {
+        if ($value !== null) {
+            throw new InvalidArgumentException('Cannot combine multiple options and include a value');
+        }
+    }
+
+    /**
+     * Split a combined short option into individual options.
+     *
+     * @param OptionType       $type The type
+     * @param non-empty-string $name The name to split
+     *
+     * @return Option[]
+     */
+    protected static function splitCombinedShortOptions(OptionType $type, string $name): array
+    {
+        $options = [];
+
+        foreach (str_split($name) as $item) {
+            /** @var non-empty-string $item */
+            $options[] = new Option(
+                name: $item,
+                type: $type
+            );
+        }
+
+        return $options;
     }
 }
