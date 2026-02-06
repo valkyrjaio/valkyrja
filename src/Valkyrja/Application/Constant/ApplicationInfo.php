6f74946dc2eb87d0eb7c220d2aeb876ba4272693
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-02-06 09:12:08 +0000

    [Release] Update version for v25.28.0 release

--- src/Valkyrja/Application/Constant/ApplicationInfo.php
+++ src/Valkyrja/Application/Constant/ApplicationInfo.php
@@ -20,14 +20,14 @@ final class ApplicationInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '25.27.0';
+    public const string VERSION = '25.28.0';
 
     /**
      * The Application framework version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'February 4 2026 23:29:02 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'February 6 2026 02:12:08 MST';
 
     /**
      * The valkyrja framework ascii art.
