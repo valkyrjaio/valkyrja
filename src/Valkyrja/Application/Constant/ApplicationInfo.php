2cb843bc5f39f254f5e29c1e4e3115d32b377c23
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-08-02 08:44:59 +0000

    [v26.8.0] chore: Update ApplicationInfo for the release

--- src/Valkyrja/Application/Constant/ApplicationInfo.php
+++ src/Valkyrja/Application/Constant/ApplicationInfo.php
@@ -20,14 +20,14 @@ final class ApplicationInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.7.0';
+    public const string VERSION = '26.8.0';
 
     /**
      * The Application framework version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'July 31 2026 08:59:17 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'August 2 2026 01:44:11 MST';
 
     /**
      * The valkyrja framework ascii art.
