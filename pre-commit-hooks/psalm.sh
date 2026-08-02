18d2cadc3513a73191fb2d29a5afe47e37ae7c94
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-02 04:18:07 -0700

    [Copyright] ci: Check the header of the shell scripts.

--- pre-commit-hooks/psalm.sh
+++ pre-commit-hooks/psalm.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#
+# This file is part of the Valkyrja Framework package.
+#
+# Copyright (c) 2016-present Melech Mizrachi
+#
+# Released under the MIT License. See LICENSE.md for details.
+#
 
 composer psalm
