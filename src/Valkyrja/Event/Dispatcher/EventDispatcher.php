3c12eff6cc117b0ac01c4832fbe676bc6afb1910
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-05 22:19:56 -0700

    [Event] docs: Restore the original summary line on the event lookup.

--- src/Valkyrja/Event/Dispatcher/EventDispatcher.php
+++ src/Valkyrja/Event/Dispatcher/EventDispatcher.php
@@ -125,7 +125,7 @@ class EventDispatcher implements EventDispatcherContract
     }
 
     /**
-     * Get the event that a given id names.
+     * Get an event class from a given id.
      *
      * @param class-string            $eventId   The event class name
      * @param array<array-key, mixed> $arguments The arguments to pass to the event class
